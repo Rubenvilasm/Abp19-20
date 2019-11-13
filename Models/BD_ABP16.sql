23d3b8e10fa987c693c08064ff9bbfbca1ca997f
Ruben <rubenvilasm@gmail.com>
2019-11-13 17:15:19 +0100

    Base de datos compilando, faltan algunas relaciones

--- Models/BD_ABP16.sql
+++ Models/BD_ABP16.sql
@@ -1,93 +1,166 @@
+--
+-- Base de datos: `PadelAbp`
+--
+CREATE DATABASE IF NOT EXISTS `PadelABP` DEFAULT CHARACTER SET latin1 COLLATE latin1_spanish_ci;
+USE `PadelABP`;
 
--- Incorporado el 10-11-2018 por autor Omega
--- 
-DROP DATABASE IF EXISTS `PADELDB`;
-CREATE DATABASE `PADELDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
---
--- SELECCIONAMOS PARA USAR
---
-USE `PADELDB`;
---
--- DAMOS PERMISO USO Y BORRAMOS EL USUARIO QUE QUEREMOS CREAR POR SI EXISTE
---
-GRANT USAGE ON *.* TO `PADELDB`@`localhost`;
-  DROP USER `PADELDB`@`localhost`;
---
--- CREAMOS EL USUARIO Y LE DAMOS PASSWORD,DAMOS PERMISO DE USO Y DAMOS PERMISOS SOBRE LA BASE DE DATOS.
---
-CREATE USER IF NOT EXISTS `PADELDB`@`localhost` IDENTIFIED BY 'ABP16';
-GRANT USAGE ON *.* TO `PADELDB`@`localhost` REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-GRANT ALL PRIVILEGES ON `PADELDB`.* TO `PADELDB`@`localhost` WITH GRANT OPTION;
 
 CREATE TABLE IF NOT EXISTS `USUARIO` (
-  LOGIN varchar(15) NOT NULL,
-  PASSWORD varchar(128) NOT NULL,
-  NOMBRE varchar(30) NOT NULL,
-  APELLIDOS varchar(50) NOT NULL,
-  DNI varchar(9) NOT NULL UNIQUE,
-  EMAIL varchar(60) NOT NULL UNIQUE,
-  TELEFONO varchar(11) NOT NULL,
-  AVATAR varchar(50) NOT NULL,
-  ACEPTADO enum('SI','NO') DEFAULT 'NO',
-  ROL enum('ADMIN','DEPORTISTA','ENTRENADOR') NOT NULL,
-  BORRADO enum('SI','NO') DEFAULT 'NO',
-  SEXO enum('MASC','FEM','OTRO'),
+  `login` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  `password` varchar(128) COLLATE latin1_spanish_ci NOT NULL,
+  `nombre` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+  `apellidos` varchar(50) COLLATE latin1_spanish_ci DEFAULT NULL,
+  `dni` varchar(10) CHARACTER SET utf8 COLLATE utf8_spanish_ci DEFAULT NULL,
+  `fechaNacimiento` date DEFAULT NULL,
+  `email` varchar(50) COLLATE latin1_spanish_ci DEFAULT NULL,
+  `telefono` int(15) DEFAULT NULL,
+  `rol` enum('Deportista','Administrador','Entrenador') COLLATE latin1_spanish_ci DEFAULT NULL,
+  `socio` enum('Activo','Inactivo') COLLATE latin1_spanish_ci DEFAULT NULL,
+  `foto` varchar(500) COLLATE latin1_spanish_ci DEFAULT NULL,
 
+  PRIMARY KEY (login)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
-PRIMARY KEY (LOGIN)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `pareja` (
+  `idPareja` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  `idDeportista1` varchar(128) COLLATE latin1_spanish_ci NOT NULL,
+  `idDeportista2` varchar(128) COLLATE latin1_spanish_ci NOT NULL,
 
+  PRIMARY KEY (idPareja)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
-
-
-CREATE TABLE IF NOT EXISTS `PISTA` (
-  ID_PISTA int(255) NOT NULL UNIQUE,
-  RESERVADA enum('SI','NO') DEFAULT 'NO',
-
-
-PRIMARY KEY (ID_PISTA)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `RESERVA_PISTA`(
-  ID_PISTA int(255) NOT NULL UNIQUE,
-  ID_RESPONSABLE int(255),
-  RESERVADA enum('SI','NO'),
-  ID_RESERVA int(255) AUTO_INCREMENT NOT NULL,
-  
-  PRIMARY KEY(ID_RESERVA),
-  FOREIGN KEY(ID_PISTA) REFERENCES PISTA (ID_PISTA)
-  
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `ENTRENADOR`(
-  ID_ENTRENADOR int(255) UNIQUE,
-  LOGIN int(255) NOT NULL,
-
-  
-
-PRIMARY KEY(ID_ENTRENADOR),
-FOREIGN KEY(LOGIN) REFERENCES USUARIO (LOGIN)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE IF NOT EXISTS `CALENDARIO`(
-  FECHA_COMPLETA varchar(10),
-  DIA int(2),
-  MES int(2),
-  AÑO int(4),
-  DIA_SEMANA enum('LUNES','MARTES','MIERCOLES','JUEVES','VIERNES','SABADO','DOMINGO'),
+  CREATE TABLE IF NOT EXISTS `pista` (
+    `idPista` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `nombre` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `especificaciones` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
     
-  PRIMARY KEY(FECHA_COMPLETA)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
+    PRIMARY KEY (idPista)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci; 
 
-CREATE TABLE IF NOT EXISTS `HORA_CALENDARIO`(
-  HORA int(2),
-  MINUTE int(2),
-  SECOND int(2),
-  HORA_COMPLETA varchar(8),
+  CREATE TABLE IF NOT EXISTS `reserva` (	      
+    `idReserva` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idPista` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idUsuario` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fecha` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
 
-  PRIMARY KEY(HORA_COMPLETA)
-)ENGINE=MyISAM DEFAULT CHARSET=utf8;
+    PRIMARY KEY (idReserva,idPista),
+    FOREIGN KEY (idPista) REFERENCES pista(idPista)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
+
+
+  CREATE TABLE IF NOT EXISTS `campeonato` (
+    `idCampeonato` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fechaInicio` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fechaFin` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `numParticipantes` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `premios` varchar(125) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `normativa` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+    PRIMARY KEY (idCampeonato)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+  CREATE TABLE IF NOT EXISTS `categoria` (
+    `idCategoria` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `nombre` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `nivel` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  
+    PRIMARY KEY (idCategoria)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+  CREATE TABLE IF NOT EXISTS `partidoPromocionado` (
+    `idPartidoPromocionado` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `nombre` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fecha` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idParticipante1` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `idParticipante2` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `idParticipante3` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `idParticipante4` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `numParticipantes` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+
+    PRIMARY KEY (idPartidoPromocionado)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  
+  CREATE TABLE IF NOT EXISTS `partido` (
+    `idPartido` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idPareja1` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idPareja2` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fecha` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `resultado` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+
+    PRIMARY KEY (idPartido,idPareja1,idPareja2),
+    FOREIGN KEY (idPista) REFERENCES pista (idPista)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+  CREATE TABLE IF NOT EXISTS `calendario` (
+    `idReserva` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `idPartido` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `idPartidoPromocionado` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+    `nombre` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `fecha` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idCampeonato` varchar(25) COLLATE latin1_spanish_ci DEFAULT NULL,
+
+    PRIMARY KEY (idReserva, idPartido),
+    FOREIGN KEY (idReserva) REFERENCES reserva (idReserva),
+    FOREIGN KEY (idPartido) REFERENCES partido (idPartido)
+
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  
+
+  
+  CREATE TABLE IF NOT EXISTS `grupo` (
+    `numParticipantes` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idGrupo` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idGanador` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+    PRIMARY KEY (idGrupo)
+
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci; 
+  
+
+  
+  CREATE TABLE IF NOT EXISTS `playOff` (
+    `idPlayOff` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idGrupo` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `eliminado` BIT DEFAULT 0,
+    `idPareja` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+    PRIMARY KEY (idPlayOff),
+    FOREIGN KEY (idPareja) REFERENCES pareja(idPareja),
+    FOREIGN KEY (idGrupo) REFERENCES grupo (idGrupo)
+  )ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+   
+
+
+ CREATE TABLE IF NOT EXISTS `publicacion` (	      
+  `idNoticia` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  `Nombre` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  `Descripcion` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+  `idAutor` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+  PRIMARY KEY (idNoticia)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+
+ CREATE TABLE IF NOT EXISTS `estadistica` (
+    `idUsuario` varchar(25) COLLATE latin1_spanish_ci NOT NULL UNIQUE,
+    `partidosGanados` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `puntosPorPartido` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `partidosPerdidos` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+    PRIMARY KEY (idUsuario),
+    FOREIGN KEY (idUsuario) REFERENCES USUARIO(idUsuario0)
+  )ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  
+  CREATE TABLE IF NOT EXISTS `ligaRegular` (
+    `idLiga` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `idGrupo` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `IdPartido` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+    `puntuacion` varchar(25) COLLATE latin1_spanish_ci NOT NULL,
+
+    PRIMARY KEY (idLiga),
+    FOREIGN KEY (idGrupo) REFERENCES GRUPO(idGrupo),
+    FOREIGN KEY (idPartido) REFERENCES partido(idPartido)
+  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
+  
 
