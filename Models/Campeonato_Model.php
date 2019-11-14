75c10431bfd6406b121da1e6a171a9593df8935c
Ruben <rubenvilasm@gmail.com>
2019-11-14 18:26:50 +0100

    Algunas inserciones en la bd

--- Models/Campeonato_Model.php
+++ Models/Campeonato_Model.php
@@ -3,6 +3,7 @@
 class Campeonato_Model{
 
     var $idCampeonato;
+    var $nombreCampeonato;
     var $fechaInicio;
     var $fechaFin;
     var $premios;
@@ -12,8 +13,9 @@ class Campeonato_Model{
 
     var $mysqli;
 
-    function __construct($idCampeonato,$fechaInicio,$fechaFin,$premios,$normativa,$numParticipantes){
+    function __construct($idCampeonato,$nombreCampeonato,$fechaInicio,$fechaFin,$premios,$normativa,$numParticipantes){
         $this->idCampeonato = $idCampeonato;
+        $this->nombreCampeonato = $nombreCampeonato;
         $this->fechaInicio = $fechaInicio;
         $this->fechaFin = $fechaFin;
         $this->premios = $premios;
@@ -35,6 +37,7 @@ class Campeonato_Model{
                 if($result->num_rows == 0){
                     $sql = "INSERT INTO campeonato (
                         idCampeonato,
+                        nombreCampeonato,
                         fechaInicio,
                         fechaFin,
                         premios,
@@ -42,6 +45,7 @@ class Campeonato_Model{
                         numParticipantes)
                                 VALUES(
                                     '$this->idCampeonato',
+                                    '$this->nombreCampeonato',
                                     '$this->fechaInicio',
                                     '$this->fechaFin',
                                     '$this->premios',
@@ -56,6 +60,64 @@ class Campeonato_Model{
         }else return 'ERROR: El atributo clave idcampeonato está vacío.'
     }
 
-    function DELETE()
+    function DELETE(){
+        $sql = "SELECT * FROM campeonato WHERE (`idCampeonato` = '$this->idCampeonato' AND `borrado` = 'NO'))";
+
+        $result = $this->mysqli->query($sql);
+
+        if($result->num_rows == 1){
+            $sql = "UPDATE campeonato SET `borrado` = 'SI' WHERE (
+                    `idCampeonato` = '$this->idCampeonato')";
+            if(!($result = $this->mysqli->query($sql))){
+                return 'ERROR: Fallo en la consulta sobre la base de datos.';
+            }else return 'El Campeonato ha sido eliminado con exito.';
+        }else return 'ERROR: No hay ningún campeonato con esa id.'
+    }
+
+    function SEARCH(){
+        $sql  = "SELECT * FROM campeonato WHERE (
+                (`idCampeonato` LIKE '%$this->idCampeonato%')AND
+                (`fechaInicio` LIKE '%$this->fechaInicio%')AND
+                (`fechaFin` LIKE '%$this->fechaFin%')AND
+                (`premios` LIKE '%$this->premios%')AND
+                (`normativa` LIKE '%$this->normativa%')AND
+                (`numParticipantes` LIKE '%$this->numParticipantes%')AND
+                (`borrado` LIKE '%$this->borrado%'))
+                )";
+
+        
+        if(!($result = $this->mysqli->query($sql))){
+            return 'ERROR: Fallo en la consulta sobre la base de datos.';
+        }else return $result;
+    }
+
+    function GET_NOMBRE_CAMPEONATOS(){
+
+        $sql = "SELECT nombre FROM campeonato WHERE(`borrado` = 'NO')";
+
+        if(!($result = $this->mysqli->query($sql))){
+            return 'ERROR: Fallo en la consulta sobre la base de datos.'
+        }else return $result;
+    }
+
+    function GET_NUMCAMPEONATOS(){
+
+        $sql = "SELECT COUNT(idCampeonato) FROM campeonato";
+
+
+        if(!($result = $this->mysqli->query($sql))){
+            return 'ERROR: Fallo en la consulta sobre la base de datos.'
+        }else return $result;
+    }
+
+    function rellenarDatos(){
+        $sql = "SELECT * FROM campeonato WHERE(`idCampeonato` = '$this->campeonato')";
+
+        if(!($result = $this->mysqli->query($sql))){
+            return 'ERROR: No existe ese campeonato en la base de datos.';
+        }else return $result;
+    }
+
+    
 }
 ?>
